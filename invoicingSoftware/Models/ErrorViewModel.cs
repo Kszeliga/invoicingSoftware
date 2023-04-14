15c25d283836a1cc632e9373c4d157963097ae10
redbu <redbu@DESKTOP-OMRDEEC>
2023-04-14 13:13:33 +0200

    Dodaj pliki projektów.

---
+++ invoicingSoftware/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace invoicingSoftware.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
